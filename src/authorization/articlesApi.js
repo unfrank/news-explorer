f8fb428ff00c980713be7a74460c6afc34f1acd4
James Unthank <james@unfrank.com>
2025-06-15 17:43:04 -0400

    Final frontend updates before repo rename

--- src/authorization/articlesApi.js
+++ src/authorization/articlesApi.js
@@ -2,21 +2,21 @@ import * as apiClient from "./apiClient";
 import { getToken } from "./auth";
 
 export function fetchAllArticles() {
-  return apiClient.get("/api/articles", getToken());
+  return apiClient.get("/articles", getToken());
 }
 
 export function fetchArticleById(id) {
-  return apiClient.get(`/api/articles/${id}`, getToken());
+  return apiClient.get(`/articles/${id}`, getToken());
 }
 
 export function createArticle(data) {
-  return apiClient.post("/api/articles", data, getToken());
+  return apiClient.post("/articles", data, getToken());
 }
 
 export function updateArticle(id, data) {
-  return apiClient.put(`/api/articles/${id}`, data, getToken());
+  return apiClient.put(`/articles/${id}`, data, getToken());
 }
 
 export function deleteArticle(id) {
-  return apiClient.del(`/api/articles/${id}`, getToken());
+  return apiClient.del(`/articles/${id}`, getToken());
 }
