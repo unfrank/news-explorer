5cbc1e4e7ddbb311297b5fb0e0985c23db4e31bf
James Unthank <james@unfrank.com>
2025-06-19 12:35:38 -0400

    preloader sem

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,7 +2,12 @@ import "./Preloader.css";
 
 function Preloader({ className }) {
   return (
-    <div className={className}>
+    <div
+      className={className}
+      role="status"
+      aria-live="polite"
+      aria-label="Loading content"
+    >
       <div className="preloader__spinner circle-preloader"></div>
     </div>
   );
