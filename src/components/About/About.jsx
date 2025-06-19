087f7c6277c4984f6762448d9569603a1fa7a9cb
James Unthank <james@unfrank.com>
2025-06-18 21:50:39 -0400

    about sem

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,4 @@
 import "./About.css";
-// import lolaImage from "../../assets/images/lola.jpg";
 import authorImage from "../../assets/images/author.jpg";
 import authorBio from "../../utils/bio.js";
 
@@ -7,18 +6,17 @@ function About() {
   return (
     <section className="about">
       <div className="layout-container">
-        <div className="about__container">
+        <article className="about__container">
           <img className="about__image" src={authorImage} alt="Author" />
           <div className="about__text">
             <h2 className="about__title">About the author</h2>
-
             {authorBio.map((paragraph, idx) => (
               <p className="about__paragraph" key={idx}>
                 {paragraph}
               </p>
             ))}
           </div>
-        </div>
+        </article>
       </div>
     </section>
   );
