2a32ce0ba61d3fba3827aaf4ad33e34862f612a8
James Unthank <james@unfrank.com>
2025-06-19 13:01:23 -0400

    All semantic tags corrected

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,3 +1,48 @@
+// import { useState } from "react";
+// import "./SearchForm.css";
+
+// function SearchForm({ onSearch }) {
+//   const [query, setQuery] = useState("");
+//   const [error, setError] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!query.trim()) {
+//       setError("Please enter a keyword");
+//       return;
+//     }
+//     setError("");
+//     onSearch(query.trim());
+//   };
+
+//   return (
+//     <form className="search-form" onSubmit={handleSubmit}>
+//       <div className="search-form__container">
+//         <input
+//           className="search-form__input"
+//           type="text"
+//           placeholder="Enter topic"
+//           value={query}
+//           onChange={(e) => setQuery(e.target.value)}
+//         />
+//         <button className="search-form__button" type="submit">
+//           Search
+//         </button>
+//       </div>
+//       <p
+//         className={`search-form__error ${
+//           error ? "search-form__error--visible" : ""
+//         }`}
+//       >
+//         {error || " "}
+//       </p>
+//     </form>
+//   );
+// }
+
+// export default SearchForm;
+
+// redo
 import { useState } from "react";
 import "./SearchForm.css";
 
@@ -18,7 +63,11 @@ function SearchForm({ onSearch }) {
   return (
     <form className="search-form" onSubmit={handleSubmit}>
       <div className="search-form__container">
+        <label htmlFor="search" className="search-form__label--hidden">
+          Search topic
+        </label>
         <input
+          id="search"
           className="search-form__input"
           type="text"
           placeholder="Enter topic"
